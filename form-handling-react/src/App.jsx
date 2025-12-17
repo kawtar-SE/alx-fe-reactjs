d1f4ba235db2a0a172f058e4bbc53ae0eee5a874
Kawtar assoulaimani <128231221+kawtar-SE@users.noreply.github.com>
2025-12-17 19:00:05 +0100

    Add files via upload

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,14 @@
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
+
+function App() {
+return (
+<div>
+<RegistrationForm />
+<FormikForm/>
+</div>
+);
+}
+
+
+export default App;
